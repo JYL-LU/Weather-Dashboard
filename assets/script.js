da1fea38ff031bf28f9f6383a31dc59502228624
Jane Lu <jyl.janelu@gmail.com>
2021-06-13 22:11:57 -0700

    basic js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 var currentDate = new Date();
 var APIkey = "d91f911bcf2c0f925fb6535547a5ddc9";
-var todayWeather = document.getElementById("today");
+var todayAndForecastWeather = document.getElementById("todayAndForecast");
 
 var searchBtn = document.getElementById("search-button");
 
-//5 day forecast
-var forecastFive = document.querySelector("#forecastAll");
-var searchHistory = document.querySelector("#history");
-var cityHistory = 1;
+var lastSearches = [];
 
-function Weather(cityName) {
+function formatDate(date) {
+  // Code from a friend who claims its a much better way to do the same thing,but i am not deleting my two hours of codes:
+  // return date.toLocaleString(undefined, {
+  //   dateStyle: "medium",
+  //   timeStyle: undefined
+  // });
+  return (
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+  );
+}
+
+function getData(cityName) {
+  // need to get latitude/longitude for the OpenWeather One Call API.
+  // starting with a city name, then use the first
+  // query to get long/lat and then a second query to get the data for it.
   let url =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
@@ -17,62 +28,152 @@ function Weather(cityName) {
     APIkey +
     "&units=imperial";
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.coord)
+    .then((coord) => {
+      let url =
+        "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
+        coord.lat +
+        "&lon=" +
+        coord.lon +
+        "&appid=" +
+        APIkey;
+      return fetch(url);
     })
-    .then((data) => {
-      console.log(data);
+    .then((response) => response.json());
+}
 
-      var currentDate = new Date();
-      //console.log(currentDate);
-      var daytag =
-        currentDate.getFullYear() +
-        "-" +
-        (currentDate.getMonth() + 1) +
-        "-" +
-        currentDate.getDate();
-      document.getElementById("today").innerHTML = daytag;
-      //console.log(daytag);
+function displayWeather(cityName, data) {
+  console.log(data);
 
-      var weatherPic = data.weather[0].icon;
-      var currentPic = document.getElementById("currentPic");
-      currentPic.setAttribute(
-        "src",
-        "https://openweathermap.org/img/w/" + weatherPic + ".png"
-      );
+  // reset container
+  while (todayAndForecastWeather.firstChild) {
+    todayAndForecastWeather.removeChild(todayAndForecastWeather.firstChild);
+  }
 
-      var cityName = data.name;
-      var nametag = document.createElement("p");
-      nametag.textContent = cityName;
+  var currentDate = new Date();
+  //console.log(currentDate);
+  var daytag = document.createElement("p");
+  daytag.textContent = formatDate(currentDate);
 
-      var tempt = data.main.temp;
-      var temptag = document.createElement("p");
-      temptag.textContent = "Temperature: " + tempt + " ºF";
+  //console.log(daytag);
 
-      var humidity = data.main.humidity;
-      var humiditytag = document.createElement("p");
-      humiditytag.textContent = "Humidity: " + humidity + "%";
+  var weatherPic = data.current.weather[0].icon;
+  var currentPic = document.createElement("img");
+  currentPic.setAttribute(
+    "src",
+    "https://openweathermap.org/img/w/" + weatherPic + ".png"
+  );
 
-      var windSpeed = data.wind.speed;
-      var windtag = document.createElement("p");
-      windtag.textContent = "Wind Speed: " + windSpeed + " MPH";
+  var nametag = document.createElement("p");
+  nametag.textContent = cityName;
 
-      //console.log(daytag, nametag, temptag, humiditytag, windtag);
-      //console.log(todayWeather);
-      //console.log(currentPic);
-      todayWeather.append(nametag, currentPic, temptag, humiditytag, windtag);
-    })
+  var tempt = data.current.temp;
+  var temptag = document.createElement("p");
+  temptag.textContent = "Temperature: " + tempt + " ºF";
 
-    .then(function (forcastResponse) {
-      for (let i = 1; i < 6; i++) {}
-    });
+  var humidity = data.current.humidity;
+  var humiditytag = document.createElement("p");
+  humiditytag.textContent = "Humidity: " + humidity + "%";
+
+  var windSpeed = data.current.wind_speed;
+  var windtag = document.createElement("p");
+  windtag.textContent = "Wind Speed: " + windSpeed + " MPH";
+
+  var uvi = data.current.uvi;
+  var uvitag = document.createElement("p");
+  uvitag.textContent = "UVI: " + uvi;
+
+  //console.log(daytag, nametag, temptag, humiditytag, windtag);
+  //console.log(todayWeather);
+  //console.log(currentPic);
+  todayAndForecastWeather.append(
+    nametag,
+    daytag,
+    currentPic,
+    temptag,
+    humiditytag,
+    windtag,
+    uvitag
+  );
+}
+
+function forecastWeather(cityName, data) {
+  var forecastTag = document.createElement("div");
+  forecastTag.id = "forecastContainer";
+
+  for (let i = 0; i < 5; i++) {
+    let dailyData = data.daily[i];
+
+    var dayTag = document.createElement("div");
+    dayTag.className = "dayBox";
+
+    var day = new Date(dailyData.dt * 1000);
+    var dateTag = document.createElement("p");
+    dateTag.textContent = formatDate(day);
+
+    var tempTag = document.createElement("p");
+    tempTag.textContent = "Temp: " + dailyData.temp.day + " ºF";
+
+    var humTag = document.createElement("p");
+    humTag.textContent = "Hum: " + dailyData.humidity + "%";
+
+    var windTag = document.createElement("p");
+    windTag.textContent = "Wind: " + dailyData.wind_speed + " MPH";
+
+    var uviTag = document.createElement("p");
+    uviTag.textContent = "UVI: " + dailyData.uvi;
+
+    dayTag.append(dateTag, tempTag, humTag, windTag, uviTag);
+    forecastTag.appendChild(dayTag);
+  }
+  todayAndForecastWeather.append(forecastTag);
+}
+
+function showWeather(cityName) {
+  getData(cityName).then((data) => {
+    displayWeather(cityName, data);
+    forecastWeather(cityName, data);
+  });
+}
+
+function showHistoryList() {
+  let container = document.getElementById("historyList");
+  // reset container
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  for (let city of lastSearches) {
+    let div = document.createElement("div");
+    let link = document.createElement("a");
+    link.href = "#";
+    link.textContent = city;
+    //()=> shedule this function
+    link.addEventListener("click", () => showWeather(city));
+    div.appendChild(link);
+    container.appendChild(div);
+  }
+}
+
+function updateHistoryList(cityName) {
+  // The maximum number of searches kept in memory.
+  let maxSearches = 5;
+  lastSearches = lastSearches.slice(0, maxSearches - 1);
+  lastSearches.unshift(cityName);
+}
+
+function onCitySelect(cityName) {
+  showWeather(cityName);
+  updateHistoryList(cityName);
+  showHistoryList();
 }
 
 function search() {
-  var citySearch = document.getElementById("city").value;
-  console.log(citySearch);
-  Weather(citySearch);
+  var cityName = document.getElementById("city").value;
+  onCitySelect(cityName);
 }
 
 searchBtn.addEventListener("click", search);
+
+showWeather("New York");
